f320047d44c6b499a2295351520eb8379aed2f6f
Drimodaren <men.a.a@yandex.ru>
2022-07-26 19:31:18 +0300

    class 17

--- arr_17/arr_17.js
+++ arr_17/arr_17.js
@@ -21,22 +21,76 @@ isBracketStructureBalanced('([)]'); // false
 Подсказки
 Решение учителя использует метод indexOf
 */
-
+const openBrackets = ["(", "[", "{", "<"];
+const closeBrackets = [")", "]", "}", ">"];
+const pairs = {
+  "(": ")",
+  "[": "]",
+  "{": "}",
+  "<": ">",
+};
 const checkIsBalanced = (expression) => {
   // Инициализация стека
-  const stack = [];
+  const stack = []; //'(','['
   // Проходим по каждому символу в строке
   for (const symbol of expression) {
     // Смотрим открывающий или закрывающий
-    if (symbol === '{' ) {
+    if (openBrackets.includes(symbol)) {
       stack.push(symbol);
-       } else if (symbol === '}') {
+    } else if (closeBrackets.includes(symbol)) {
       // Если для закрывающего не нашлось открывающего, значит баланса нет
-      if (!stack.pop()) {
-                return false;
+      // if (!stack.pop()) {
+      //           return false;
+      // }
+      const arr = stack.pop();
+      if (!arr) {
+        return false;
+      }
+      console.log(pairs[arr]);
+      if (symbol !== pairs[arr]) {
+        return false;
       }
     }
   }
   return stack.length === 0;
 };
-console.log(checkIsBalanced('{}}'));
+console.log(checkIsBalanced("([)]"));
+
+const arr = [1, 2, 3];
+console.log(arr);
+const arr1 = arr.pop();
+console.log(arr1);
+console.log(arr);
+const arr2 = arr.pop();
+console.log(arr2);
+console.log(arr);
+const arr3 = arr.pop();
+console.log(arr3);
+console.log(arr);
+const arr4 = arr.pop();
+console.log(!arr4);
+console.log(arr);
+
+// const openingSymbols = ['(', '[', '{', '<'];
+// const closingSymbols = [')', ']', '}', '>'];
+
+// // BEGIN
+// const isOpeningSymbol = (symbol) => openingSymbols.includes(symbol);
+// const getClosingSymbolFor = (symbol) => closingSymbols[openingSymbols.indexOf(symbol)];
+
+// export default (str) => {
+//   const stack = [];
+//   for (const symbol of str) {
+//     if (isOpeningSymbol(symbol)) {
+//       const closingSymbol = getClosingSymbolFor(symbol);
+//       stack.push(closingSymbol);
+//     } else {
+//       const lastSavedSymbol = stack.pop();
+//       if (symbol !== lastSavedSymbol) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
