8671037f8025d946372d22c662be24c45f80e77c
Drimodaren <men.a.a@yandex.ru>
2022-11-01 11:45:13 +0400

    Ob_4 (Domain)

--- Object/Ob_4(Domain).js
+++ Object/Ob_4(Domain).js
@@ -1,7 +1,7 @@
-// Реализуйте и экспортируйте по умолчанию функцию, 
+// Реализуйте и экспортируйте по умолчанию функцию,
 // которая принимает на вход имя сайта и возвращает информацию о нем:
 // import getDomainInfo from './domain.js';
- 
+
 // // Если домен передан без указания протокола,
 // // то по умолчанию берется http
 // getDomainInfo('yandex.ru')
@@ -9,9 +9,25 @@
 // //   scheme: 'http',
 // //   name: 'yandex.ru',
 // // }
- 
+
 // getDomainInfo('https://hexlet.io');
 // // {
 // //   scheme: 'https',
 // //   name: 'hexlet.io',
 // // }
+
+const getDomainInfo = (domainInfo) => {
+  let scheme = '';
+  if (domainInfo.startsWith('https')) {
+    scheme = 'https';
+  } else {
+    scheme = 'http';
+  }
+  const parts = domainInfo.split('/');
+  const name = parts[parts.length - 1];
+  const info = { scheme: scheme, name: name };
+
+  return info;
+};
+
+console.log(getDomainInfo('https://hexlet.io'));
